2272509c881cb13fe3677744b14d305c440a492c
Taqi Raza <mttalpur@gmail.com>
2021-07-04 00:53:42 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -58,7 +58,7 @@ wineboot ; sleep 5
 
 cp -Rvp $WINEPREFIX npp-stable/ ; rm -rf $WINEPREFIX
 
-( cd npp-stable ; wget -qO- 'https://gist.github.com/mmtrt/df659de58e36ee091e203ab3c1460619/raw/0b79a2c514c940645312447a6db6371e0ce3376b/nppswp.patch' | patch -p1 )
+( cd npp-stable ; wget -qO- 'https://gist.github.com/mmtrt/df659de58e36ee091e203ab3c1460619/raw/c5a6c78c7412f10d4d2a548930c3f1e54223a019/nppswp.patch' | patch -p1 )
 
 export ARCH=x86_64; squashfs-root/AppRun -v ./npp-stable -n -u "gh-releases-zsync|mmtrt|notepad-plus-plus_AppImage|stable_wp|notepad*.AppImage.zsync" notepad-plus-plus_"${ver}"_WP-${ARCH}.AppImage
 
